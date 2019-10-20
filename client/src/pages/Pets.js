ec3a422c178ea68ec6cd6c2d8578f17616d41c24
Amir Seljubac <amir_seljubac@hotmail.com>
2019-10-20 13:52:30 +0200

    Add query and mutation

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -5,15 +5,50 @@ import PetsList from '../components/PetsList'
 import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
 
+const ALL_PETS = gql`
+  query AllPets {
+    pets {
+      id
+      name
+      type
+      img
+    }
+  }
+`
+
+const NEW_PET = gql`
+	mutation CreateAPets($newPet: NewPetInput!) {
+    addPet(input: $newPet) {
+      id
+      name
+      type
+      img
+    }
+  }
+`
 
 export default function Pets () {
   const [modal, setModal] = useState(false)
-
+  const { data, loading, error} = useQuery(ALL_PETS)
+  const [ createPet, newPet ] = useMutation(NEW_PET)
 
   const onSubmit = input => {
     setModal(false)
+    createPet({ 
+      variables: {
+        newPet: input
+      }
+    })
   }
   
+  if (loading || newPet.loading) {
+    return <Loader />
+  }
+
+  if (error || newPet.error) {
+    return <p>error!</p>
+  }
+
   if (modal) {
     return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
   }
@@ -32,7 +67,7 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets}/>
       </section>
     </div>
   )
